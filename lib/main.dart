aa2ab9f58459e9274f3ab260cab9b923998bff71
Mohamad Hassan <m7md.7asn1@gmail.com>
2023-05-24 17:47:32 +0300

    final

--- lib/main.dart
+++ lib/main.dart
@@ -9,26 +9,32 @@ void main() {
 
 class FoodZilla extends StatelessWidget {
   const FoodZilla({super.key});
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-        theme: ThemeData.dark().copyWith(
-            elevatedButtonTheme: ElevatedButtonThemeData(
-              style: ButtonStyle(
-                foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-                ),
-                minimumSize: MaterialStateProperty.all(Size(150, 60)),
+      // Setting the theme for the entire application
+      theme: ThemeData.dark().copyWith(
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          // Customizing the style of elevated buttons
+          style: ButtonStyle(
+            foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(50),
               ),
             ),
-            appBarTheme: AppBarTheme(backgroundColor: Colors.black12),
-            colorScheme: ColorScheme.dark(primary: Colors.deepPurple
-                // Replace with your desired primary color
-                )),
-        debugShowCheckedModeBanner: false,
-        home: SplashBody());
+            minimumSize: MaterialStateProperty.all(Size(150, 60)),
+          ),
+        ),
+        appBarTheme: AppBarTheme(backgroundColor: Colors.black12),
+        colorScheme: ColorScheme.dark(
+          primary: Colors.deepPurple,
+          // Replace with your desired primary color
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: SplashBody(), // Setting the initial page to be displayed
+    );
   }
 }
--- lib/pages/restaurantsPage/restaurentPage.dart
+++ lib/pages/restaurantsPage/restaurentPage.dart
@@ -9,23 +9,26 @@ class RestaurantsPage extends StatelessWidget {
 
   // Fetch restaurant IDs from the database
   Future<List<int>> getRestsId() async {
+    // Retrieve the restaurant IDs from the 'restaurant' table in the database
     List<int> restIds = await db.getTableIds('restId', 'restaurant');
     return restIds;
   }
 
   @override
   Widget build(BuildContext context) {
+    // Get a future list of Restaurant objects based on restaurant IDs
     Future<List<Restaurant>> restListFuture =
         getRestsId().then((restIds) => db.getRestaurantDetailsList(restIds));
 
     return Scaffold(
       appBar: AppBar(
-          centerTitle: true,
-          title: Text(
-              "Restaurants List")), // Corrected the word "Restaurents" to "Restaurants"
+        centerTitle: true,
+        title: Text("Restaurants List"),
+      ),
       body: Container(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
+          // Display the list of restaurants using the RestaurantList widget
           child: RestaurantList(restListFuture),
         ),
       ),
